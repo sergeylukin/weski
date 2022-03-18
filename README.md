e4b96f3a0c08503e25d8dd7fb0596da194a79417
Sergey Lukin <contact@sergeylukin.com>
2022-03-18 11:21:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hotels search app
+
+In order to bootstrap the project, run `./init.sh` in terminal
---
+++ init.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+npx create-nx-workspace@latest weski \
+  --appName=web-app \
+  --preset=react-express \
+  --npmScope=weski \
+  --nx-cloud=false \
+  --linter=eslint \
+  --style=scss && \
+cd weski/ && \
+npm i axios --force && \
+npm i -D json-server --force && \
+npm i -D concurrently --force && \
+npm i @material-ui/core --force && \
+nx g lib core-data --component=false && \
+nx g lib core-state --component=false && \
+nx g lib material --component=false && \
+nx g slice resorts \
+    --project core-state \
+    --directory resorts \
+    --no-interactive \
+    --facade && \
+nx g slice offers \
+    --project core-state \
+    --directory offers \
+    --no-interactive \
+    --facade && \
+nx g c resorts  --export=false --routing=true --style=@emotion/styled && \
+nx g c resorts-list --directory=resorts --export=false --routing=true --style=@emotion/styled && \
+nx g c resort-details  --directory=resorts  --export=false --routing=true --style=@emotion/styled && \
+nx g c offers  --export=false --routing=true --style=@emotion/styled && \
+nx g c offers-list --directory=offers --export=false --routing=true --style=@emotion/styled && \
+nx g c offer-details  --directory=offers  --export=false --routing=true --style=@emotion/styled && \
+nx g c home  --export=false --routing=true --style=@emotion/styled && \
+mkdir server && touch server/db.json && \
+npx concurrently "npm start" "npm start api"
